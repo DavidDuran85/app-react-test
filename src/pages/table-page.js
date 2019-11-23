4562d0c77d39c2576992a2f6d18a4cef19df7dfb
DavidDur <davidduranvaldes8512@yahoo.com.mx>
2019-11-23 12:31:25 -0600

    App Tabs

---
+++ src/pages/table-page.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class TablePage extends Component{
+    render(){
+        return ( <div>
+            Page table
+        </div>)
+    }
+}
+
+export default TablePage;
